69e49ce47a454803b038453b70c8a27cee76fc6a
Tom Keffer <tkeffer@gmail.com>
2018-01-02 13:22:44 -0800

    Experimenting with webpack + react

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
+import 'babel-polyfill'
+
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import todoApp from './reducers'
-import App from './components/App'
-
-let store = createStore(todoApp)
+import Root from './containers/Root'
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Root />,
     document.getElementById('root')
 )
