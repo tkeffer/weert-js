898f65246cd0bd2d9614d70db59be5a6dc8dfdf4
Tom Keffer <tkeffer@gmail.com>
2018-02-04 17:07:49 -0700

    Tuned plots

--- client/src/components/PlotGroup.js
+++ client/src/components/PlotGroup.js
@@ -16,13 +16,26 @@ const propTypes = {
     obsTypes         : PropTypes.arrayOf(PropTypes.string),
     aggregation      : PropTypes.number,
     isFetching       : PropTypes.bool,
+    animationDuration: PropTypes.number,
+    dot              : PropTypes.bool,
+    margin           : PropTypes.shape({
+                                           top   : PropTypes.number,
+                                           right : PropTypes.number,
+                                           left  : PropTypes.number,
+                                           bottom: PropTypes.number
+                                       }),
+    stroke           : PropTypes.string,
     componentClass   : PropTypes.string,
 };
 
 const defaultProps = {
-    obsTypes      : ["wind_speed", "sealevel_pressure", "out_temperature", "in_temperature"],
-    isFetching    : false,
-    componentClass: 'div',
+    obsTypes         : ["wind_speed", "sealevel_pressure", "out_temperature", "in_temperature"],
+    isFetching       : false,
+    animationDuration: 500,
+    dot              : false,
+    margin           : {top: 5, right: 30, left: 20, bottom: 5},
+    stroke           : '#8884d8',
+    componentClass   : 'div',
 };
 
 export default class PlotGroup extends React.PureComponent {
@@ -38,10 +51,18 @@ export default class PlotGroup extends React.PureComponent {
 
     render() {
         const {
-                  selectedTimeScale, packets, obsTypes,
-                  aggregation, isFetching, componentClass: Component
-              } = this.props;
-        const timeFormatter = (tick) => {return d3.timeFormat('%H:%M:%S')(new Date(tick))};
+                  selectedTimeScale,
+                  packets,
+                  obsTypes,
+                  aggregation,
+                  isFetching,
+                  animationDuration,
+                  dot,
+                  margin,
+                  stroke,
+                  componentClass: Component
+              }             = this.props;
+        const timeFormatter = (tick) => {return d3.timeFormat('%H:%M:%S')(new Date(tick));};
 
         // TODO: Need buttons to change detail
         return (
@@ -55,18 +76,24 @@ export default class PlotGroup extends React.PureComponent {
                      {aggregation && <h4>({aggregation} minute aggregation)</h4>}
 
                      <h4> Selected detail: {this.state.selectedDetail}</h4>
-                     {/* Include a key. See https://reactjs.org/docs/reconciliation.html#keys */}
                      {obsTypes.map((obsType, i) => {
                          return (
-                             <LineChart width={600} height={300} data={packets}
-                                        margin={{top: 5, right: 30, left: 20, bottom: 5}} key={obsType}>
-                                 <XAxis dataKey="timestamp" scale="time" tickFormatter={timeFormatter}/>
-                                 <YAxis/>
-                                 <CartesianGrid strokeDasharray='3 3' />
-                                 <Tooltip labelFormatter={timeFormatter}/>
-                                 <Legend/>
-                                 <Line type="monotone" dataKey={obsType} stroke="#8884d8" activeDot={{r: 8}}/>
-                             </LineChart>
+                             <div>
+                                 <h4>{obsType}</h4>
+                                 <LineChart width={600} height={300} data={packets}
+                                            margin={margin} key={obsType}>
+                                     <XAxis dataKey='timestamp' scale='time' tickFormatter={timeFormatter}/>
+                                     <YAxis/>
+                                     <CartesianGrid strokeDasharray='3 3'/>
+                                     <Tooltip labelFormatter={timeFormatter}/>
+                                     <Line type='linear'
+                                           dataKey={obsType}
+                                           stroke={stroke}
+                                           animationDuration={animationDuration}
+                                           dot={dot}
+                                           animationEasing='linear'/>
+                                 </LineChart>
+                             </div>
                          );
                      })}
                  </div>}
