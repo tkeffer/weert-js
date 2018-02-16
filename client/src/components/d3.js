cfad3703db472eb0a838f88c1a2ad36228a2aa16
Tom Keffer <tkeffer@gmail.com>
2018-02-16 14:05:02 -0800

    Removed some no longer needed D3 dependencies

--- client/src/components/d3.js
+++ client/src/components/d3.js
@@ -1,18 +1,14 @@
 /*
  * This allows references to d3 utilities to be done using the familiar d3 tag notation.
  */
-import * as interpolate from "d3-interpolate";
-import * as transition from "d3-transition";
-import * as selection from "d3-selection";
 import * as ease from "d3-ease";
+import * as interpolate from "d3-interpolate";
 import * as scale from "d3-scale";
-import {timeFormat} from "d3-time-format";
+import * as transition from "d3-transition";
 
 export default {
-    ...interpolate,
-    ...transition,
-    ...selection,
     ...ease,
+    ...interpolate,
     ...scale,
-    timeFormat
+    ...transition,
 };
