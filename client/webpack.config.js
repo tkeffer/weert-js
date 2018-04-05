09ec173b43909229897ea9648816df451c1821c5
Tom Keffer <tkeffer@gmail.com>
2018-04-05 14:17:07 -0700

    Transitioned to Webpack 4

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,10 +5,51 @@
  */
 
 /*
- * The default Webpack configuration is the development configuration.
+ * Webpack configuration options common to both production and development versions.
  */
 
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = merge(common, { devtool: "inline-source-map" });
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  module: {
+    // NB: Before Webpack 2, "rules" was called "modules".
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    // This will inject the necessary <script> tag, with a link to the bundle, into the index.html file
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      inject: "body"
+    })
+
+    // Comment out to include moment locales, uncomment to exclude them.
+    // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+    // Very useful to profile the size of included modules:
+    // new BundleAnalyzerPlugin(),
+  ]
+};
