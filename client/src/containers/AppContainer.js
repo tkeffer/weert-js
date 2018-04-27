d21d36f5dafe8b02fbff95ba7caf1f6082414266
Tom Keffer <tkeffer@gmail.com>
2018-04-27 16:03:21 -0700

    Simplified dropdown, as well as fixed warning

--- client/src/containers/AppContainer.js
+++ client/src/containers/AppContainer.js
@@ -63,8 +63,7 @@ class AppContainer extends React.PureComponent {
 
     // Get statistics appropriate for the selected time span. Time span 'recent' always shows
     // the day's statistics.
-    const selectedStats =
-      selectedTimeSpan === "recent" ? "day" : selectedTimeSpan;
+    const selectedStats = selectedTimeSpan === "recent" ? "day" : selectedTimeSpan;
     dispatch(fetchStatsIfNeeded(selectedStats));
   }
 
@@ -82,8 +81,7 @@ class AppContainer extends React.PureComponent {
     if (selectedTimeSpan !== prevProps.selectedTimeSpan) {
       this.fetchAndSubscribeIfNeeded(selectedTimeSpan);
       const { dispatch } = this.props;
-      const selectedStats =
-        selectedTimeSpan === "recent" ? "day" : selectedTimeSpan;
+      const selectedStats = selectedTimeSpan === "recent" ? "day" : selectedTimeSpan;
       dispatch(fetchStatsIfNeeded(selectedStats));
     }
   }
@@ -105,9 +103,7 @@ class AppContainer extends React.PureComponent {
     // Before subscribing, check to see if we already have a subscription for this series
     if (!this.state.subscriptions[measurement]) {
       // Subscribe to any new packets coming from the given series
-      const subscription = dispatch(
-        subscribeMeasurement(measurement, selectedTags)
-      );
+      const subscription = dispatch(subscribeMeasurement(measurement, selectedTags));
       // Save the new subscription object. It will be needed to cancel the subscription.
       this.setState({
         ...this.state,
@@ -127,11 +123,7 @@ class AppContainer extends React.PureComponent {
     const currentPacket = recentState.packets.slice(-1)[0];
     const isFetchingCurrentPacket = recentState.isFetching;
 
-    const {
-      packetTableOptions,
-      windCompassOptions,
-      statsTableOptions
-    } = this.state;
+    const { packetTableOptions, windCompassOptions, statsTableOptions } = this.state;
 
     let selectedStatsSpan, selectedStats;
     if (selectedTimeSpan === "recent") {
@@ -160,12 +152,8 @@ class AppContainer extends React.PureComponent {
             <div>
               <WindCompass
                 {...windCompassOptions}
-                windSpeed={
-                  currentPacket ? currentPacket["wind_speed"] : undefined
-                }
-                windDirection={
-                  currentPacket ? currentPacket["wind_dir"] : undefined
-                }
+                windSpeed={currentPacket ? currentPacket["wind_speed"] : undefined}
+                windDirection={currentPacket ? currentPacket["wind_dir"] : undefined}
                 isFetching={isFetchingCurrentPacket}
               />
             </div>
@@ -183,27 +171,23 @@ class AppContainer extends React.PureComponent {
           </Col>
 
           <Col xs={12} lg={9}>
-            <Tabs
+            <Nav
+              bsStyle="tabs"
               activeKey={this.state.selectedTimeSpan}
               onSelect={this.handleChange}
             >
-              <Nav bsStyle="tabs">
-                <NavDropdown eventKey="recent" title="Recent...">
-                  <MenuItem eventKey="recent.5">5 minutes</MenuItem>
-                  <MenuItem eventKey="recent.10">10 minutes</MenuItem>
-                  <MenuItem eventKey="recent.30">30 minutes</MenuItem>
-                  <MenuItem eventKey="recent.60">60 minutes</MenuItem>
-                </NavDropdown>
-                <NavItem eventKey="day">Day</NavItem>
-                <NavItem eventKey="week">Week</NavItem>
-                <NavItem eventKey="month">Month</NavItem>
-                <NavItem eventKey="year">Year</NavItem>
-              </Nav>
-            </Tabs>
-            <PlotContainer
-              selectedTimeSpan={selectedTimeSpan}
-              selectedState={selectedState}
-            />
+              <NavDropdown eventKey="recent" title="Recent..." id="recent-dropdown">
+                <MenuItem eventKey="recent.5">5 minutes</MenuItem>
+                <MenuItem eventKey="recent.10">10 minutes</MenuItem>
+                <MenuItem eventKey="recent.30">30 minutes</MenuItem>
+                <MenuItem eventKey="recent.60">60 minutes</MenuItem>
+              </NavDropdown>
+              <NavItem eventKey="day">Day</NavItem>
+              <NavItem eventKey="week">Week</NavItem>
+              <NavItem eventKey="month">Month</NavItem>
+              <NavItem eventKey="year">Year</NavItem>
+            </Nav>
+            <PlotContainer selectedTimeSpan={selectedTimeSpan} selectedState={selectedState} />
           </Col>
         </Row>
       </Grid>
