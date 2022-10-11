3675f967a8cdcb1b12cbcc5d247eee13a58b357d
Tom Keffer <tkeffer@gmail.com>
2022-10-11 06:15:31 -0700

    Get rid of obsolete componentWillReceiveProps()
    Allowed simplifying the component

--- client/src/components/ObsValue.js
+++ client/src/components/ObsValue.js
@@ -5,7 +5,6 @@
  */
 
 import React from "react";
-import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 import * as units from "../units";
@@ -24,51 +23,20 @@ const defaultProps = {
   componentClass: "div",
 };
 
-export default class ObsValue extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { oldString: "", key: true };
-  }
+export default function ObsValue(props) {
+  const { obsType, value, unitSystem, format, isFetching, componentClass: Component } = props;
 
-  componentWillReceiveProps(newProps) {
-    // Check to see whether the new props will change the resultant HTML. If it does,
-    // toggle "key".
-    const newString = units.getValueString(
-      newProps.obsType,
-      newProps.value,
-      newProps.unitSystem,
-      newProps.format
-    );
-    const oldString = ReactDOM.findDOMNode(this).innerHTML;
-    const key = oldString === newString ? this.state.key : !this.state.key;
-    this.setState({ oldString, key });
-  }
+  const newString = isFetching
+    ? "Loading"
+    : units.getValueString(obsType, value, unitSystem, format);
 
-  render() {
-    const {
-      obsType,
-      value,
-      unitSystem,
-      format,
-      isFetching,
-      componentClass: Component,
-    } = this.props;
-
-    const newString = isFetching
-      ? "Loading"
-      : units.getValueString(obsType, value, unitSystem, format);
-
-    // Apply a "fade in" if the resultant string has changed.
-    const cn = this.state.oldString === newString ? "" : "fadeIn";
-
-    // Include the toggled key. This will cause React to think the component is a new component, thus
-    // forcing a new animation and the fade in.
-    return (
-      <Component key={this.state.key} className={cn}>
-        {newString}
-      </Component>
-    );
-  }
+  // Use the string as they key. If it changes, React will recognize it as a new component
+  // that needs to be rerendered, causing the fade-in.
+  return (
+    <Component key={newString} className="fadeIn">
+      {newString}
+    </Component>
+  );
 }
 
 ObsValue.propTypes = propTypes;
